a34c7bf11cc8bf196cd770e5012955f670972af5
Rajith <rajith.venu-gopal@mocaclinic.ca>
2021-02-19 20:34:40 -0500

    update

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 let dbURI = 'mongodb://localhost/Loc8r'; //local DB connection
 
 if (process.env.NODE_ENV === 'production') {
-    dbURI = 'mongodb+srv://rajith:rajith0627@cluster0.qnjpb.mongodb.net/loc8r?retryWrites=true&w=majority'; //Remote DB connection
+    dbURI = process.env.MONGODB_URI; //Remote DB connection
 }
 
 mongoose.connect(dbURI, { useNewUrlParser: true });
