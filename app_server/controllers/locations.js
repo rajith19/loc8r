73797c7b7f15b7468cf65278dde1347bb395a396
Rajith <rajith.venu-gopal@mocaclinic.ca>
2021-03-18 17:16:36 -0400

    remote url

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,6 +3,11 @@ const apiOptions = {
   server: 'http://localhost:3000'
 };
 
+if (process.env.NODE_ENV === 'production'){
+
+apiOptions.server = "https://loc8rrj.herokuapp.com";  //remote DB connection
+}
+
 
 const formatDistance = (distance) => {
   let thisDistance = 0;
