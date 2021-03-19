0df21e2a4b7c5a41dbf5507874d5a41712e59d4c
Rajith <rajith.venu-gopal@mocaclinic.ca>
2021-03-18 20:11:51 -0400

    angular implementation

---
+++ app_public/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HomeListComponent } from './home-list/home-list.component';
+import { DistancePipe } from './distance.pipe';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    HomeListComponent,
+    DistancePipe
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [HomeListComponent]
+})
+export class AppModule { }
